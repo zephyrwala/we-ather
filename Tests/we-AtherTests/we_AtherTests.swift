694cc01cd768af8ed3cda2da287edbac226ed26f
Mayur P <64838833+zephyrwala@users.noreply.github.com>
2024-01-14 20:14:23 +0530

    Initial Commit

---
+++ Tests/we-AtherTests/we_AtherTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import we_Ather
+
+final class we_AtherTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
